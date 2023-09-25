59ea1f8b102a5d0a71984ca95753876c97ccc910
Ruphasa <rfrizqifauzan@gmail.com>
2023-09-25 14:24:22 +0700

    + Nilai Huruf

--- PemilihanPercobaan224.java
+++ PemilihanPercobaan224.java
@@ -4,6 +4,7 @@ public class PemilihanPercobaan224 {
 
     public static void main(String[] args) {
         Scanner input24 = new Scanner(System.in);
+        String nilaiHuruf;
         System.out.println("Nilai UAS   : ");
         float UAS = input24.nextFloat();
         System.out.println("Nilai UTS   : ");
@@ -15,5 +16,27 @@ public class PemilihanPercobaan224 {
         float total = (UAS*0.4F)+(UTS*0.3F)+(Quis*0.2F)+(Tugas*0.1F);
         String message = total < 65 ? "Remidi":"Tidak remidi";
         System.out.println("Nilai Akhir = "+total+" Sehingga "+message);
+        if (total <= 39) {
+            nilaiHuruf = "E";
+            System.out.println("Kamu berada di grade "+nilaiHuruf);
+        }else if(total <= 50){
+            nilaiHuruf = "D";
+            System.out.println("Kamu berada di grade "+nilaiHuruf);
+        }else if(total<=60){
+            nilaiHuruf = "C";
+            System.out.println("Kamu berada di grade "+nilaiHuruf);
+        }else if(total<=65){
+            nilaiHuruf = "C+";
+            System.out.println("Kamu berada di grade "+nilaiHuruf);
+        }else if(total<=73){
+            nilaiHuruf = "B";
+            System.out.println("Kamu berada di grade "+nilaiHuruf);
+        }else if(total<=80){
+            nilaiHuruf = "B+";
+            System.out.println("Kamu berada di grade "+nilaiHuruf);
+        }else if(total<=100){
+            nilaiHuruf = "A";
+            System.out.println("Kamu berada di grade "+nilaiHuruf);
+        }
     }
 }
